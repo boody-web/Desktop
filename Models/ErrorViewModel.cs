a1d263a467276753ebce86b04588e48befb8eff9
Boody Tamer <Boody546911@outlook.com>
2024-09-01 08:30:59 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace html_css.lecture3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
